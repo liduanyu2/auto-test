a9ce2002985a9d72747ae7481f6d3bd6057aeba9
lnight_xiu <1057111817@qq.com>
2020-06-20 18:41:41 +0800

    6月20号从公司上传:完成自动关闭修改密码

--- test/test/start_browser.py
+++ test/test/start_browser.py
@@ -1,14 +1,52 @@
 # coding=utf-8
-import time
-from selenium import webdriver
+import time #延时
+from selenium import webdriver #webdriver
+from selenium.webdriver.support import expected_conditions as EC #检测元素可见
+from selenium.webdriver.support.wait import WebDriverWait #持续操作时间
+from selenium.webdriver.common.by import By #捕获元素
 
-driver1 = webdriver.Chrome()
-driver1.get("http://hcloud.bszhihui.com")
-time.sleep(1)
-driver1.find_element_by_class_name("imgmb").click()
-time.sleep(1)
-frame = driver1.find_element_by_xpath("//*[@id=\"cboxLoadedContent\"]/iframe")
-driver1.switch_to_frame(frame)
-driver1.find_element_by_id("userName").send_keys("2076793")
-driver1.find_element_by_id("passWord").send_keys("111111")
-driver1.find_element_by_id("SubmitBt").click()
+driver = webdriver.Chrome()
+user_name = 2076793
+pass_word = 111111
+#初始化driver
+
+#浏览器初始化
+def driver_init():
+    driver.get("http://hcloud.bszhihui.com")
+    driver.maximize_window()
+    time.sleep(1)
+
+#获取element信息
+def get_element_classname(class_name):
+    element = driver.find_element_by_class_name(class_name)
+    return element
+
+def get_element_xpath(xpath):
+    element = driver.find_element_by_xpath(xpath)
+    return element
+
+def get_element_id(id):
+    element = driver.find_element_by_id(id)
+    return element
+
+#检测更改密码
+def close_changePassWord():
+    get_element_id()
+
+def run_main():
+    driver_init()
+    get_element_classname("imgmb").click()
+    time.sleep(1)
+    driver.switch_to_frame(get_element_xpath("//*[@id=\"cboxLoadedContent\"]/iframe"))#跳转到frame元素
+    get_element_id("userName").send_keys(user_name)
+    get_element_id("passWord").send_keys(pass_word)
+    get_element_id("SubmitBt").click()
+
+    locator=(By.CLASS_NAME,"cboxIframe")
+    if WebDriverWait(driver,3).until(EC.presence_of_element_located(locator)):
+        driver.switch_to_frame(get_element_xpath("//*[@id=\"cboxLoadedContent\"]/iframe"))
+        driver.find_element_by_xpath("/html/body/div[1]/div[1]/a").click()
+
+
+run_main()
+
